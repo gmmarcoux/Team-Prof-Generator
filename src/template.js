3f6bddaa2c0208732a0d53af60c82d63e4ee382a
Grace Marcoux <gmmarcoux@me.com>
2022-07-20 09:37:17 -0500

    general format? of template

---
+++ src/template.js
@@ -0,0 +1,27 @@
+//A. Generate the team's HTML formt
+const generateTeam = team => {
+
+    //1. MANAGER
+
+
+
+    //2. INTERNS
+
+
+
+    //3. ENGINNEERS 
+
+
+
+
+    //4. THE WHOLE PAGE
+
+
+
+
+
+
+
+
+
+}
